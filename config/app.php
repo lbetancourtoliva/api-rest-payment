d3fce86106a98b0b8f162fd221cd61b46398452c
luis <lbetancourtoliva@gmail.com>
2020-08-07 10:54:49 -0400

    project skeleton | complete installing laravel-datatables

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash' => Laracasts\Flash\Flash::class,
+        'Datatables' => Yajra\Datatables\Facades\Datatables::class,
 
     ],
 
